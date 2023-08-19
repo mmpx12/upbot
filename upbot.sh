10b088ba214a20d83693cff45aeb636ce23ceb19
vinz <mmpx09@protonmail.com>
2023-08-19 17:59:30 +0000

    fixed false positive

--- upbot.sh
+++ upbot.sh
@@ -7,26 +7,39 @@ cd "$(dirname "$0")" || exit 1
 FREE_ID=""
 FREE_TOKEN=""
 DOMAIN=""
+DOMAIN_SLUG=""
 
 touch upbot.run
 
-status_code="$(curl -s -o /dev/null -I -w "%{http_code}" "$DOMAIN")"
+status_code="$(curl -sIo /dev/null -m 7 -w "%{http_code}" "$DOMAIN")"
 
 
-function SendAlert(){
+function DownAlert(){
   curl -s --get --data "user=$FREE_ID" --data "pass=$FREE_TOKEN" \
-    --data-urlencode "msg=[$DOMAIN] Down for $1 min" \
+    --data-urlencode "msg=[$DOMAIN_SLUG] Down for $1 min" \
+    "https://smsapi.free-mobile.fr/sendmsg"
+}
+
+function UpAlert(){
+  curl -s --get --data "user=$FREE_ID" --data "pass=$FREE_TOKEN" \
+    --data-urlencode "msg=[$DOMAIN_SLUG] recovered (down for $1 min)." \
     "https://smsapi.free-mobile.fr/sendmsg"
 }
 
 
 if [[ -f upbot.down ]]; then
+  if [[ "$status_code" -eq 200 ]]; then
+    UpAlert "$(cat upbot.down)"
+    rm -f upbot.down upbot.run
+    (echo -ne "Up "; date) >> down.log;
+    exit 0
+  fi
   count="$(cat upbot.down)"
   time=0
   if [[ "$count" -ge 2 ]]; then
     touch upbot.alert
     until [[ ! -f upbot.alert ]]; do
-      SendAlert "$time"
+      DownAlert "$time"
       time="$(echo "$time + 0.5" | bc)"
       sleep 30
     done
